0bf09122a0ec49a73ca39b727603d976d07a15e4
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-05-11 03:12:51 +0530

    Basic Layout

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <h1>About our nuxt</h1>
+    <p class="subheading">They are aajuicy and stuff</p>
+    <p>Lorem ipsum dolor, sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt modi esse, necessitatibus soluta ullam deleniti? Fugiat sapiente dolores accusamus recusandae distinctio, quisquam quia iusto sint repellendus nesciunt, minus ullam. Soluta. consectetur adipisicing elit. Autem, voluptatibus consequuntur animi dolore assumenda sequi earum adipisci nobis explicabo numquam obcaecati aut culpa! Nam sunt, possimus maxime harum at quidem?</p>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About Nuxt",
+      meta: [
+        { name: "description", content: "This is my about description here." },
+        { name: "keywords", content: "about nuxt, nuxt info" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <h1>The Best Nuxt</h1>
+    <p class="subheading">We've gor the best nuxt's.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, voluptatibus consequuntur animi dolore assumenda sequi earum adipisci nobis explicabo numquam obcaecati aut culpa! Nam sunt, possimus maxime harum at quidem?</p>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Nuxt Home",
+      meta: [
+        { name: "description", content: "This is my Nuxt Home." },
+        { name: "keywords", content: "about nuxt, nuxt info" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
